ea702f2eba91283b73632400740b7de8af83c749
Faiyaz <faiyaz.b.mohd@gmail.com>
2023-03-15 21:26:47 +0530

    some changes were made

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ connectToMongo();
 const express = require('express')
 const cors = require('cors') 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
